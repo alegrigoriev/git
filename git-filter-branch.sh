alegrigoriev/git

commit 71400d97b12abeba25f2e8245a9751daf8583142

filter-branch: make report-progress more readable
The name of some variables that are used very locally in this
function were overly long; they were making the lines harder to read
and the longer names didn't add much more information.

Signed-off-by: Junio C Hamano <gitster@pobox.com>

git-filter-branch.sh
@@ -280,18 +280,20 @@ report_progress ()
 	if test -n "$progress" &&
 		test $git_filter_branch__commit_count -gt $next_sample_at
 	then
-		now_timestamp=$(date +%s)
-		elapsed_seconds=$(($now_timestamp - $start_timestamp))
-		remaining_second=$(( ($commits - $git_filter_branch__commit_count) * $elapsed_seconds / $git_filter_branch__commit_count ))
-		if test $elapsed_seconds -gt 0
+		count=$git_filter_branch__commit_count
+
+		now=$(date +%s)
+		elapsed=$(($now - $start_timestamp))
+		remaining=$(( ($commits - $count) * $elapsed / $count ))
+		if test $elapsed -gt 0
 		then
-			next_sample_at=$(( ($elapsed_seconds + 1) * $git_filter_branch__commit_count / $elapsed_seconds ))
+			next_sample_at=$(( ($elapsed + 1) * $count / $elapsed ))
 		else
 			next_sample_at=$(($next_sample_at + 1))
 		fi
-		progress=" ($elapsed_seconds seconds passed, remaining $remaining_second predicted)"
+		progress=" ($elapsed seconds passed, remaining $remaining predicted)"
 	fi
-	printf "\rRewrite $commit ($git_filter_branch__commit_count/$commits)$progress    "
+	printf "\rRewrite $commit ($count/$commits)$progress    "
 }
 
 git_filter_branch__commit_count=0

commit 6bd175842b87075670e0ae3e85c28400926b3e7c

filter-branch: progress reporting fixes.
Update commit count at every progress line print
(that was the original way, regressed in 71400d97)

Don't do report_progress if we're skipping a comit
present in the map restored by --state-branch from
a previous run. There could be tens of thousands of them,
why waste time.

Factor out those skipped commits when
calculating the estimation for remaining time.

Give the remaining time calculation a few more seconds (5) to settle
(instead of just one second).

git-filter-branch.sh
@@ -327,26 +327,32 @@ test $commits -eq 0 && die_with_status 2 "Found nothing to rewrite"
 # Rewrite the commits
 report_progress ()
 {
+	local count effective_count now elapsed remaining
+	count=$git_filter_branch__commit_count
+	effective_count=$(($count - $git_filter_branch__skipped_commits))
+
 	if test -n "$progress" &&
-		test $git_filter_branch__commit_count -gt $next_sample_at
+		test $count -gt $next_sample_at
 	then
-		count=$git_filter_branch__commit_count
 
 		now=$(date +%s)
 		elapsed=$(($now - $start_timestamp))
-		remaining=$(( ($commits - $count) * $elapsed / $count ))
-		if test $elapsed -gt 0
+		if test $elapsed -gt 5 && test $effective_count -gt 0
 		then
-			next_sample_at=$(( ($elapsed + 1) * $count / $elapsed ))
+			next_sample_at=$(( ($elapsed + 1) * $effective_count / $elapsed ))
+			remaining=$(( ($commits - $count) * $elapsed / $effective_count ))
+			progress=" ($elapsed seconds passed, remaining $remaining predicted)"
 		else
 			next_sample_at=$(($next_sample_at + 1))
+			progress=" ($elapsed seconds passed)"
 		fi
-		progress=" ($elapsed seconds passed, remaining $remaining predicted)"
 	fi
 	printf "\rRewrite $commit ($count/$commits)$progress    "
 }
 
 git_filter_branch__commit_count=0
+# Number of commits skipped because they've been processed in previous invocation of filter-branch
+git_filter_branch__skipped_commits=0
 
 progress= start_timestamp=
 if date '+%s' 2>/dev/null | grep -q '^[0-9][0-9]*$'
@@ -371,8 +377,8 @@ eval "$filter_setup" < /dev/null ||
 while read commit parents; do
 	git_filter_branch__commit_count=$(($git_filter_branch__commit_count+1))
 
+	test -f "$workdir"/../map/$commit && git_filter_branch__skipped_commits=$(($git_filter_branch__skipped_commits+1)) continue
 	report_progress
-	test -f "$workdir"/../map/$commit && continue
 
 	case "$filter_subdir" in
 	"")